f47272c68728ba44fb4e2632e7f98a3ab992d515
MD Moon Babu <m.mmoon1527@gmail.com>
2026-04-07 23:56:57 +0600

    Added Header and my premade css

---
+++ src/Header.jsx
@@ -0,0 +1,55 @@
+function Header() {
+  return (
+    <header>
+      <nav className="navbar">
+        <div className="nav-brand">
+          Amine_codes<span className="brand-dot">.</span>
+        </div>
+        <ul className="nav-menu">
+          <li>
+            <a href="#home" className="nav-link active">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="nav-link">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#skills" className="nav-link">
+              Skills
+            </a>
+          </li>
+          <li>
+            <a href="#service" className="nav-link">
+              Services
+            </a>
+          </li>
+          <li>
+            <a href="#project" className="nav-link">
+              Projects
+            </a>
+          </li>
+          <li>
+            <a href="#contact" className="nav-link">
+              Contact
+            </a>
+          </li>
+        </ul>
+        <div className="nav-cta">
+          <a href="#contact" className="cta-btn">
+            Let's Talk
+          </a>
+        </div>
+        <div classNameName="hamburger">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
